0d7cc855a0dd6d4578c2f73010df3609aa56f8a0
Ridvan Yigit <ridvanyigit@gmx.net>
2026-08-24 13:33:09 +0200

    chore: project bootstrap with SwiftUI template

---
+++ AIPhotographyCoach/AIPhotographyCoach/AIPhotographyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPhotographyCoachApp.swift
+//  AIPhotographyCoach
+//
+//  Created by Ridvan Yigit on 24.08.26.
+//
+
+import SwiftUI
+
+@main
+struct AIPhotographyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIPhotographyCoach/AIPhotographyCoach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIPhotographyCoach
+//
+//  Created by Ridvan Yigit on 24.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
