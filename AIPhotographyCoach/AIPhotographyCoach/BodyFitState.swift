4b1bdee1eb13d3ce7b7b88c7e55a5729ad940f14
Ridvan Yigit <ridvanyigit@gmx.net>
2026-08-28 13:42:35 +0200

    refactor: remove all legacy modes (Pano, Spatial, FullBody, Portrait, Beauty) to focus entirely on ultra-smart Selfie mode

--- AIPhotographyCoach/AIPhotographyCoach/BodyFitState.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-// MARK: - Boydan Çekim İskelet ve Altın Oran Uyum Durumları
-enum BodyFitState: String, CaseIterable {
-    case searching = "Searching Subject..."
-    case moveCloser = "Move Closer 🔍"
-    case stepBack = "Step Back 👣"
-    case perfectFit = "Perfect Fit ✨"
-}
